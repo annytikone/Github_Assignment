ef443f356850761ce187a81fe84d1b4a8e0bc1b1
Aniket <anikettikone9@gmail.com>
2021-07-21 16:51:11 +0530

    feat:created router

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,7 +2,6 @@ import express from 'express';
 
 const router = express.Router();
 
-// http://localhost:8080/v1/?userHandler=annytikone
 router.get('/', (req, res) => {
   res.json('Router is working');
 });
